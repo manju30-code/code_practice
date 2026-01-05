224e4801848b88db3eded4129f9bea1ee5ddda15
Manju <manjukmoorthi@gmail.com>
2026-01-05 15:14:46 +0530

    wordBreak DP: Refactor wordBreak

--- src/main/java/dp/WordBreak.java
+++ src/main/java/dp/WordBreak.java
@@ -4,15 +4,30 @@ import java.util.Arrays;
 import java.util.List;
 
 class WordBreak {
-    public static boolean wordBreak(String s, List<String> wordDict) {
+    public static boolean wordBreak(String s, List<String> wordDict, int start, int [] status) {
 
-        return false;
+        if(start == s.length())
+            return true;
+
+        if (status[start] != 0) {
+            return status[start] == 1;
+        }
+
+        for(int i = start+1; i <= s.length(); i++){
+            String left = s.substring(start,i);
+            if(wordDict.contains(left) && wordBreak(s, wordDict,i,status)){
+                return status[start] == 1;
+            }
+        }
+        return status[start] == 0;
     }
 
     public static void main (String[] args){
-        String testString = "leetcode";
-        List<String> wordDict = Arrays.asList("leet", "code");
-        boolean result = wordBreak(testString, wordDict);
+        String testString = "catsandog";
+        List<String> wordDict = Arrays.asList("cats", "cat","sand","dog");
+        int [] status = new int[testString.length()];
+        int start = 0;
+        boolean result = wordBreak(testString, wordDict,start,status);
         System.out.println("Can break: " + result);
     }
 }
